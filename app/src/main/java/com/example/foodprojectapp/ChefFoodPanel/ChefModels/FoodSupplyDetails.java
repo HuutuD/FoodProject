5313c470c53ca98bdb9d9b0178f19ed5c44c6ccf
HuuTuD <danghuutu309@gmail.com>
2024-11-02 19:08:21 +0700

    CRUD Dishes

--- app/src/main/java/com/example/foodprojectapp/ChefFoodPanel/ChefModels/FoodSupplyDetails.java
+++ app/src/main/java/com/example/foodprojectapp/ChefFoodPanel/ChefModels/FoodSupplyDetails.java
@@ -2,14 +2,13 @@ package com.example.foodprojectapp.ChefFoodPanel.ChefModels;
 
 public class FoodSupplyDetails {
 
-    public String Dishes,Quantity,Price,Description,ImageURL,RandomUID,ChefId;
+    public String Dishes,Quantity,Price,Description,RandomUID,ChefId;
 
-    public FoodSupplyDetails(String dishes, String quantity, String price, String description, String imageURL,String randomUID,String chefId) {
+    public FoodSupplyDetails(String dishes, String quantity, String price, String description,String randomUID,String chefId) {
         Dishes = dishes;
         Quantity = quantity;
         Price = price;
         Description = description;
-        ImageURL = imageURL;
         RandomUID=randomUID;
         ChefId=chefId;
     }
